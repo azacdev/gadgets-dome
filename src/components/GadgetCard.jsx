b9de8222b92cfd8b85b577c9317bc134912c71f6
azacdev <azacdev@gmail.com>
2023-07-16 01:51:27 +0100

    fixed image width

--- src/components/GadgetCard.jsx
+++ src/components/GadgetCard.jsx
@@ -24,20 +24,18 @@ const GadgetCard = ({ data }) => {
         theme ? "bg-light-black" : "bg-light"
       } relative flex md:flex-col justify-evenly rounded-md w-96 sm:w-80 md:w-60 shadow-xl overflow-hidden mb-4`}
     >
-      <div className="flex items-center bg-white justify-center py-2 md:py-4 h-48 overflow-hidden">
+      <div className="flex items-center bg-white justify-center p-2 md:py-4 h-48 overflow-hidden">
         <Link to={id && `/gadget/${id}`}>
           <img
             src={img}
             alt={title}
-            className="w-full md:w-36 object-cover duration-200 hover:scale-110"
+            className="w-36 md:w-36 object-cover duration-200 hover:scale-110"
           />
         </Link>
       </div>
       <div className="flex flex-col justify-evenly gap-5 sm:gap-3 flex-grow w-1/2 md:w-full p-4">
         <Link to={id && `/gadget/${id}`}>
-          <h2 className="text-xl capitalize truncate">
-            {title}
-          </h2>
+          <h2 className="text-xl capitalize truncate">{title}</h2>
           <p>${price}</p>
         </Link>
         <div className="card-actions justify-center">
