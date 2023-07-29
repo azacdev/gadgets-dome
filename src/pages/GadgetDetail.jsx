7f4c27c157ab751e89c97ec611b1e14c37af6f6c
azacdev <azacdev@gmail.com>
2023-07-29 17:20:43 +0100

    fixed: scroll restoration

--- src/pages/GadgetDetail.jsx
+++ src/pages/GadgetDetail.jsx
@@ -37,59 +37,60 @@ const GadgetDetail = () => {
     addItem(gadgetDetails);
   };
 
+  if (gadgetDetails.length === 0)
+    return (
+      <svg
+        className="svg-animate"
+        version="1.1"
+        id="L6"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 100 100"
+        enableBackground="new 0 0 100 100"
+        xmlSpace="preserve"
+      >
+        <rect
+          fill="none"
+          stroke="#666666"
+          strokeWidth="4"
+          x="25"
+          y="25"
+          width="50"
+          height="50"
+        >
+          <animateTransform
+            attributeName="transform"
+            dur="0.5s"
+            from="0 50 50"
+            to="180 50 50"
+            type="rotate"
+            id="strokeBox"
+            attributeType="XML"
+            begin="rectBox.end"
+          />
+        </rect>
+        <rect x="27" y="27" fill="#666666" width="46" height="50">
+          <animate
+            attributeName="height"
+            dur="1.3s"
+            attributeType="XML"
+            from="50"
+            to="0"
+            id="rectBox"
+            fill="freeze"
+            begin="0s;strokeBox.end"
+          />
+        </rect>
+      </svg>
+    );
+
+
   return (
     <div className="h-screen">
-      {gadgetDetails.length === 0 && (
-        <svg
-          className="svg-animate"
-          version="1.1"
-          id="L6"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 100 100"
-          enableBackground="new 0 0 100 100"
-          xmlSpace="preserve"
-        >
-          <rect
-            fill="none"
-            stroke="#666666"
-            strokeWidth="4"
-            x="25"
-            y="25"
-            width="50"
-            height="50"
-          >
-            <animateTransform
-              attributeName="transform"
-              dur="0.5s"
-              from="0 50 50"
-              to="180 50 50"
-              type="rotate"
-              id="strokeBox"
-              attributeType="XML"
-              begin="rectBox.end"
-            />
-          </rect>
-          <rect x="27" y="27" fill="#666666" width="46" height="50">
-            <animate
-              attributeName="height"
-              dur="1.3s"
-              attributeType="XML"
-              from="50"
-              to="0"
-              id="rectBox"
-              fill="freeze"
-              begin="0s;strokeBox.end"
-            />
-          </rect>
-        </svg>
-      )}
       <div className="containerWrap flex flex-col lg:flex-row p-4 md:p-16 gap-8">
-        <div
-          className="max-w-lg rounded-lg overflow-hidden"
-        >
+        <div className="max-w-lg rounded-lg overflow-hidden">
           <img src={img} className="w-full rounded-md" alt={title} />
         </div>
 
